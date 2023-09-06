83108aecac8a79a11344866df0cb4a2e00b5c2dd
hhristo1500 <72629943+h.hristo1500@users.noreply.github.com>
2023-09-06 19:38:35 +0300

    strupe test payment added

---
+++ EventOganizer/Controllers/StripeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace EventOganizer.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class StripeController : ControllerBase
+        {
+            [HttpPost("CreatePaymentIntent")]
+            public ActionResult CreatePaymentIntent([FromBody] Dictionary<string, int> data)
+            {
+                int amount = data["amount"];
+
+                if (amount <= 0)
+                {
+                    return BadRequest("Invalid amount.");
+                }
+            amount *=100;
+                StripeConfiguration.ApiKey = "sk_test_51NmMkNA82S8GS4aYkW1GcDoQiy30bNoDaG9975FOqB7bzbTqrF9KAe5HCO0dNRwtnLp0hRYztD1i9I25ygcK9pNh00CFrsdMq6";
+
+                var options = new PaymentIntentCreateOptions
+                {
+                    Amount = amount,
+                    Currency = "bgn",
+                    PaymentMethodTypes = new List<string> { "card" },
+                };
+
+                var service = new PaymentIntentService();
+                PaymentIntent paymentIntent = service.Create(options);
+
+                return Ok(new { clientSecret = paymentIntent.ClientSecret });
+            }
+
+
+            [HttpPost("PaymentSuccess")]
+            public ActionResult PaymentSuccess([FromBody] Dictionary<string, string> json)
+            {
+                var paymentIntentId = json["paymentIntentId"];
+                
+                return Ok();
+            }
+        }
+    }
+
